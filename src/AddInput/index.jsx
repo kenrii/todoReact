aceeac3ef7454da54703f3437151d5d56befbb29
Ken <47590652+kenrii@users.noreply.github.com>
2020-03-26 19:46:57 +0200

    moving elements dependency

--- src/AddInput/index.jsx
+++ src/AddInput/index.jsx
@@ -1,24 +1,25 @@
 import React, { useState } from "react";
-import './input.css';
+import "./input.css";
 
 // Todo-listalle tehdään uusia merkintöjä input-elementin avulla.
 // Elementtiin tehdään testikenttä, joka tallentaa sisältönsä
 // state-hookiin, ja nappi, joka lukee sisällön ja kutsuu
 // komponenttiin propseissa tuotua addListItem-funktiota.
 export function AddInput(props) {
-  const [input, setInput] = useState({})
-  const handleInputValue = (e) => setInput({
-    text: e.target.value
-  })
+  const [input, setInput] = useState({});
+  const handleInputValue = e =>
+    setInput({
+      text: e.target.value
+    });
 
   // Tee tähän kontrolloitu tekstikenttä ja lisää-nappi.
   // Mallia voit katsoa esimerkiksi tästä blogipostauksesta:
   // https://dev.to/stanleyjovel/simplify-controlled-components-with-react-hooks-23nn
   // Tyylittelyyn löytyy luokkia oheisesta css-tiedostosta.
   return (
-    <div class="input-container">
-      <input className="input-field" type="text" onChange = {handleInputValue} />
-      <button onClick = {() => props.addListItem(input)}> Add todo </button>
+    <div className="input-container">
+      <input className="input-field" type="text" onChange={handleInputValue} />
+      <button onClick={() => props.addListItem(input)}> Add todo </button>
     </div>
   );
 }
